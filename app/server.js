162b84d2ada61cc9e6d99e625630fccff7a497d0
onlyil <lin.cao@beibei.com>
2019-12-19 15:55:42 +0800

    Add: package.json

--- app/server.js
+++ app/server.js
@@ -1,12 +1,12 @@
 const http = require('http')
-const https = require('https')
-const pem = require('https-pem')
+// const https = require('https')
+// const pem = require('https-pem')
 
 const port = 8081
 
 const start = (route) => {
     const app = http.createServer(route)
-    const apps = https.createServer(pem, route)
+    // const apps = https.createServer(pem, route)
 
     app.listen(port, (err) => {
         if (err) {
@@ -15,12 +15,12 @@ const start = (route) => {
         console.log(`HTTP server is runing on ${port}`)
     })
 
-    apps.listen(443, (err) => {
-        if (err) {
-            console.log(err)
-        }
-        console.log('HTTPS server is runing on 443')
-    })
+    // apps.listen(443, (err) => {
+    //     if (err) {
+    //         console.log(err)
+    //     }
+    //     console.log('HTTPS server is runing on 443')
+    // })
 }
 
 exports.start = start
